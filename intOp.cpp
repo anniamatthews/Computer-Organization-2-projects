9206a98f6841c3f8e39c6781a977c9707e50e48f
anniamatthews <98440326+anniamatthews@users.noreply.github.com>
2023-01-17 19:21:23 -0500

    specified plan for functions and added variables

--- intOp.cpp
+++ intOp.cpp
@@ -8,12 +8,15 @@
 using namespace std; 
 
 int main(){
-    
+    long int left = 10; 
+    long int right = 15; 
 
     return 0; 
 }
 
 void add(long int a, long int b){
+    // plan is to perform the necessary operations and then print how long it took to do each 
+    
 } 
 // need to verify how exactly I am storing variables, 
 // but this is a start for all functions in this program until I can clarify this 
