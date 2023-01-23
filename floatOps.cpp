aa0d9f4dbc65815d6ed0b423fd17dced1c2aadd0
anniamatthews <98440326+anniamatthews@users.noreply.github.com>
2023-01-23 10:47:27 -0500

    Added timing element to record execution time

--- floatOps.cpp
+++ floatOps.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <stdio.h>
+#include<time.h>
 using namespace std; 
 
 int main(){
     double a = 0; 
     double b = 1; 
+    clock_t tStart = clock(); // start the clock 
     for(long int i = 0; i < 1e10;i++){ //additions
         a+=b;   // adding b into a 
     }
@@ -14,6 +16,8 @@ int main(){
     for(long int i = 0; i < 2e9;i++){ //divisions 
         a/=b;
     }
+    // stop recording time and print execution time 
+    printf("Time taken: %.2fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
 
 
     return 0; 
