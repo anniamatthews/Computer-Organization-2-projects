014b0239508b64b0d1d9155a4c93ae0ae9a513f8
anniamatthews <98440326+anniamatthews@users.noreply.github.com>
2023-01-23 10:41:45 -0500

    Added loops for addition, multiplication, division

--- floatOps.cpp
+++ floatOps.cpp
@@ -3,6 +3,17 @@
 using namespace std; 
 
 int main(){
+    double a = 0; 
+    double b = 1; 
+    for(long int i = 0; i < 1e10;i++){
+        a+=b;   
+    }
+    for(long int i = 0; i < 5e9;i++){
+        a*=b;
+    }
+    for(long int i = 0; i < 2e9;i++){
+        a/=b;
+    }
 
 
     return 0; 
