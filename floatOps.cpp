aa9ff3217521445de59f0ede6e40a94109b5ff25
anniamatthews <98440326+anniamatthews@users.noreply.github.com>
2023-01-23 10:42:35 -0500

    added comments

--- floatOps.cpp
+++ floatOps.cpp
@@ -5,13 +5,13 @@ using namespace std;
 int main(){
     double a = 0; 
     double b = 1; 
-    for(long int i = 0; i < 1e10;i++){
-        a+=b;   
+    for(long int i = 0; i < 1e10;i++){ //additions
+        a+=b;   // adding b into a 
     }
-    for(long int i = 0; i < 5e9;i++){
+    for(long int i = 0; i < 5e9;i++){ // multiplications
         a*=b;
     }
-    for(long int i = 0; i < 2e9;i++){
+    for(long int i = 0; i < 2e9;i++){ //divisions 
         a/=b;
     }
 
