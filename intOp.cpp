24cedd8ea128473d00fe573c668a73332a06512e
anniamatthews <98440326+anniamatthews@users.noreply.github.com>
2023-01-22 20:54:33 -0500

    started over to try new method for recording time

--- intOp.cpp
+++ intOp.cpp
@@ -4,25 +4,15 @@
 // 5*10^9 multiplications of integer constants
 // 2*10^9 divisions of integer constants 
 #include <iostream>
-#include <chrono>
 #include <stdio.h>
 using namespace std; 
 
+
 int main(){
-    long int left = 10; 
-    long int right = 15; 
+
+    
 
     return 0; 
 }
 
-void ops(long int a, long int b){
-    for(int i = 0; i < 1e10; i++){
-        a + b; 
-    }
-    for(int i = 0; i < 5*(1e9);i++){
-        a * b; 
-    }
-    for(int i = 0; i < 2*(1e9);i++){
-        a / b; 
-    }
-}
+
