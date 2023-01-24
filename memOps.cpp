99aeb988b43a77820f71ddb378abe2855b919988
anniamatthews <98440326+anniamatthews@users.noreply.github.com>
2023-01-24 13:04:21 -0500

    made array, ran iteration, and added timer element

--- memOps.cpp
+++ memOps.cpp
@@ -4,6 +4,18 @@
 using namespace std; 
 
 int main(){
+    clock_t t1, t2; 
+    int readTemp; 
+    int* arr = new int[5e9];
+    t1 = clock();
+    for(long long int i = 0; i < 5e9; i++){
+        readTemp = arr[i];
+    }
+    t2 = clock();
+    float diff((float)t2-(float)t1);
+    float seconds = diff / CLOCKS_PER_SEC;
+    cout << "Time taken:" << seconds << "s" << endl; 
+    
 
 
     return 0; 
