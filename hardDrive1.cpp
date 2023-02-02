63ecdc0fdba74078d963535af1e4975a7ed06edf
anniamatthews <98440326+anniamatthews@users.noreply.github.com>
2023-02-02 16:20:47 -0500

    added data to buffer + wrote to file, then closed

--- hardDrive1.cpp
+++ hardDrive1.cpp
@@ -8,8 +8,18 @@ int main(){
 
     // define buffer of 100 bytes
     char buffer[100];
-    
-     
+
+     for(int i = 0; i < 1000000000/100;i++){
+        // fill buffer with data
+        for(int j = 0; j < 100;j++){
+            buffer[j] = i % 26 + 'a';
+        }
+
+        //write contents of buffer to file 
+        file.write(buffer,100); 
+
+     }
+     file.close();
 
 
 
