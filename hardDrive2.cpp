62187f926116a88717e5cee818dcd0d7946c7188
anniamatthews <98440326+anniamatthews@users.noreply.github.com>
2023-02-02 18:13:29 -0500

    set up program with includes

---
+++ hardDrive2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include <time.h>
+using namespace std; 
+
+/*
+this program is very similar to that of hardDrive1.cpp 
+because we are changing the amount of bytes we are working with at a time to be 10000 instead of 100
+*/
+
+int main(){
+
+
+
+    return 0; 
+}
