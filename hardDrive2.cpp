f558db7110f62162b763c0d8243d4abe1e869f3b
anniamatthews <98440326+anniamatthews@users.noreply.github.com>
2023-02-02 18:18:48 -0500

    generated file, created buffer, and added data

--- hardDrive2.cpp
+++ hardDrive2.cpp
@@ -10,6 +10,22 @@ because we are changing the amount of bytes we are working with at a time to be 
 
 int main(){
 
+    // generate binary file "file.bin"
+    fstream file("file.bin",ios::binary | ios::out); 
+
+    // define buffer of 10000 bytes
+    char buffer[10000];
+
+    // write loop 
+    for(int i = 0; i < 1000000000/10000;i++){ // working with 10k bytes at a time so the execution should be a lot faster 
+
+        // fill buffer with data
+        for(int j = 0; j < 10000;j++){
+            buffer[j] = i % 26 + 'a'; // random information 
+        }
+        
+
+    }
 
 
     return 0; 
