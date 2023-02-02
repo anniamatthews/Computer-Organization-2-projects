2b3f5b5af6a60e2750ae94dd351fdbade1813275
anniamatthews <98440326+anniamatthews@users.noreply.github.com>
2023-02-02 17:13:36 -0500

    Added loop to read from file specified # of times

--- hardDrive1.cpp
+++ hardDrive1.cpp
@@ -10,6 +10,8 @@ int main(){
     // define buffer of 100 bytes
     char buffer[100];
 
+
+    // write loop 
     for(int i = 0; i < 1000000000/100;i++){  
     //loop will run 1000000000 times, working with 100 bytes every iteration
     
@@ -21,6 +23,13 @@ int main(){
         
         file.seekp(i*100); // update position indicator 
         file.write(buffer,100); // write contents of bufer to file
+     }
+
+
+     // read loop
+     for(int i = 0; i < 1000000000/100;i++){
+        file.seekg(i*100); // update position indicator
+        file.read(buffer,100); // read from file, 100 bytes at a time
      }
      
 
