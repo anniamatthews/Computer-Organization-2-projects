da24ac1068abce75283d3fa2101fbd54a362cb20
anniamatthews <98440326+anniamatthews@users.noreply.github.com>
2023-02-02 17:08:55 -0500

    updated write loop w/ position indicator

--- hardDrive1.cpp
+++ hardDrive1.cpp
@@ -3,27 +3,26 @@
 using namespace std; 
 
 int main(){
+    
     // generate binary file "file.bin"
     ofstream file("file.bin",ios::binary | ios::out);
 
     // define buffer of 100 bytes
     char buffer[100];
 
-     for(int i = 0; i < 1000000000/100;i++){
+    for(int i = 0; i < 1000000000/100;i++){  
+    //loop will run 1000000000 times, working with 100 bytes every iteration
+    
         // fill buffer with data
         for(int j = 0; j < 100;j++){
-            buffer[j] = i % 26 + 'a';
+            buffer[j] = i % 26 + 'a'; // random information 
         }
 
-        //write contents of buffer to file 
-        file.write(buffer,100); 
-
+        
+        file.seekp(i*100); // update position indicator 
+        file.write(buffer,100); // write contents of bufer to file
      }
-     file.close();
-
-
-
-
+     
 
     return 0; 
 }
