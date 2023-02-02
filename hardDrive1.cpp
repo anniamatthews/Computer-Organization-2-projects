c0aa768c1a04d34cb5d3cfc62ff71107eeb0681f
anniamatthews <98440326+anniamatthews@users.noreply.github.com>
2023-02-02 16:16:14 -0500

    created file and defined buffer

--- hardDrive1.cpp
+++ hardDrive1.cpp
@@ -3,6 +3,13 @@
 using namespace std; 
 
 int main(){
+    // generate binary file "file.bin"
+    ofstream file("file.bin",ios::binary | ios::out);
+
+    // define buffer of 100 bytes
+    char buffer[100];
+    
+     
 
 
 
