fc3fd81110f9ab59d76525023452cf0d251c25b0
anniamatthews <98440326+anniamatthews@users.noreply.github.com>
2023-02-03 13:56:23 -0500

    added time element

--- floatOps.cpp
+++ floatOps.cpp
@@ -6,6 +6,11 @@ using namespace std;
 int main(){
     // this program is a slightly modified version of the integer operations benchmark 
 
+    // start timer: 
+    clock_t t1, t2; 
+    int readTemp;
+    t1 = clock();
+
     // create two variables to perform operations on 
     double left = 0; // left hand operand 
     double right = 1; // right hand operand
@@ -29,8 +34,16 @@ int main(){
     for(long int i = 0; i < K;i++){  
         left/=right;
     }
+
+    // end clock 
+    t2 = clock();
+
+    // calculate time
+    float diff((float)t2-(float)t1);
+    float seconds = diff / CLOCKS_PER_SEC;
+
+    // print time taken 
+    cout << "Time taken:" << seconds << "s" << endl; 
     
-
-
     return 0; 
 }
