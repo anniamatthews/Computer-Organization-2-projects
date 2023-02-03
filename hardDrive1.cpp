06b593a4d98c4e0e0b305b35592d182ec5fdc3bb
anniamatthews <98440326+anniamatthews@users.noreply.github.com>
2023-02-03 14:02:49 -0500

    cleaned up code and comments

--- hardDrive1.cpp
+++ hardDrive1.cpp
@@ -4,42 +4,54 @@
 using namespace std; 
 
 int main(){
+    
+    // start clock 
     clock_t t1, t2; 
     int readTemp;
     t1 = clock();
+    
     // generate binary file "file.bin"
-    fstream file("file.bin",ios::binary | ios::out); 
-    //fstream to accomodate both reading and writing to the file 
+    fstream file("file.bin",ios::binary | ios::out); //fstream to accomodate both reading and writing to the file 
 
     // define buffer of 100 bytes
     char buffer[100];
 
-
     // write loop 
     for(int i = 0; i < 1000000000/100;i++){  
-    //loop will run 1000000000 times, working with 100 bytes every iteration
+        //loop will run 1000000000 times, working with 100 bytes every iteration
     
         // fill buffer with data
         for(int j = 0; j < 100;j++){
             buffer[j] = i % 26 + 'a'; // random information 
         }
 
+        // update position indicator 
+        file.seekp(i*100); 
+
+        // write contents of bufer to file, 100 bytes at a time 
+        file.write(buffer,100); 
+
+    }
+
+    // read loop
+    for(int i = 0; i < 1000000000/100;i++){
         
-        file.seekp(i*100); // update position indicator 
-        file.write(buffer,100); // write contents of bufer to file
-     }
+        // update position indicator 
+        file.seekg(i*100); 
 
+        // read from file, 100 bytes at a time 
+        file.read(buffer,100); 
+    }
 
-     // read loop
-     for(int i = 0; i < 1000000000/100;i++){
-        file.seekg(i*100); // update position indicator
-        file.read(buffer,100); // read from file, 100 bytes at a time
-     }
+    // end clock 
+    t2 = clock();
 
-        t2 = clock();
-        float diff((float)t2-(float)t1);
-        float seconds = diff / CLOCKS_PER_SEC;
-        cout << "Time taken:" << seconds << "s" << endl; 
+    // calculate time
+    float diff((float)t2-(float)t1);
+    float seconds = diff / CLOCKS_PER_SEC;
+
+    // print time taken 
+    cout << "Time taken:" << seconds << "s" << endl;
      
 
     return 0; 
