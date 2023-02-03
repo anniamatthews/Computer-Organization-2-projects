f01ca1bc2bdeea43364ddd1fc08499d825a088b2
anniamatthews <98440326+anniamatthews@users.noreply.github.com>
2023-02-03 18:03:45 -0500

    looped through array to read values

--- memOps.cpp
+++ memOps.cpp
@@ -13,13 +13,19 @@ int main(){
         memcpy(array + i, 'abcd', 4);
     }
 
-    
+    // read from array 
+    for(int i = 0; i < N; i += 4){
+        
+        // temporary buffer to work with current 4 bytes
+        char buffer[5]; // size 5 to offset the null terminator 5th element
 
+        // copy 4 bytes of data from array starting at index 'i' to the buffer
+        memcpy(buffer, array+i, 4); 
 
+        buffer[4] = '\0';
+    }
 
-
-
-    
-
+    // deallocate array of memory
+    delete[] array; 
     return 0; 
 }
