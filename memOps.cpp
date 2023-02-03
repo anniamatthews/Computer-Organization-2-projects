40094414fa8d22bcbd645bca53bf780e67e93389
anniamatthews <98440326+anniamatthews@users.noreply.github.com>
2023-02-03 17:48:32 -0500

    Iterated through array to write values

--- memOps.cpp
+++ memOps.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
-#include <stdio.h>
 #include <time.h>
-#include <vector>
 using namespace std; 
 
 int main(){
+    // define array of type char with size 5e9
+    constexpr int N = 5000000000;
+    char *array = new char[N]; // dynamically allocate memory for the array to handle large size 
+
+    // write into array 
+    for(int i = 0; i < N; i += 4){
+        // write 4 byte string 'abcd' into array 4 bytes at a time
+        memcpy(array + i, 'abcd', 4);
+    }
+
+    
+
+
+
+
+
     
 
     return 0; 
