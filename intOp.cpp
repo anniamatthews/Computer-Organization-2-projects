da19db4cca1d7ef2d44cff6d2c874196edd29af7
anniamatthews <98440326+anniamatthews@users.noreply.github.com>
2023-02-03 12:36:51 -0500

    added timing element

--- intOp.cpp
+++ intOp.cpp
@@ -10,23 +10,46 @@ using namespace std;
 
 
 int main(){
+    // start timer: 
+    clock_t t1, t2; 
+    int readTemp;
+    t1 = clock();
+    
     // create two variables to perform operations on 
     int left = 0; // left hand operand 
     int right = 1; // right hand operand
+    
+    // set # of iterations for loops 
     const int N = 10000000000; // number of additions
     const int M = 5000000000; // number of multiplications
     const in K = 2000000000; // number of divisions
     
-    for(long int i = 0; i < N; i++){ // additions
-        a+=b;
-        
+    // additions
+    for(long int i = 0; i < N; i++){ 
+        a+=b;  
     }
-    for(long int i = 0; i < M;i++){ // multiplications
+
+    // multiplications
+    for(long int i = 0; i < M;i++){ 
         a*=b; 
     }
-    for(long int i = 0; i < K; i++){ // divisions 
+
+    // divisions
+    for(long int i = 0; i < K; i++){ 
         a/=b;
     }
+
+    // end clock 
+    t2 = clock();
+
+    // calculate time
+    float diff((float)t2-(float)t1);
+    float seconds = diff / CLOCKS_PER_SEC;
+
+    // print time taken 
+    cout << "Time taken:" << seconds << "s" << endl; 
+
+
     
 
     return 0; 
