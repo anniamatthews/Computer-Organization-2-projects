12d00576c8c443200c0ccaa5c7b645ed4f5560a0
anniamatthews <98440326+anniamatthews@users.noreply.github.com>
2023-02-05 18:02:04 -0500

    started clock after variable declaration

--- hardDrive2.cpp
+++ hardDrive2.cpp
@@ -9,16 +9,16 @@ this program looks very similar to hardDrive1 because we are doing the same thin
 
 int main(){
 
-    // start clock 
-    clock_t t1, t2; 
-    int readTemp;
-    t1 = clock();
-
     // generate binary file "file2.bin"
     fstream file("file2.bin",ios::binary | ios::out); 
 
     // define buffer of 10000 bytes
     char buffer[10000];
+
+    // start clock 
+    clock_t t1, t2; 
+    int readTemp;
+    t1 = clock();
 
     // write loop 
     for(int i = 0; i < 1000000000/10000;i++){ // working with 10k bytes at a time so the execution should be a lot faster 
